28f3e51049c3ef8774d4fc612d2df822cc131370
sanjay.kannan <sanjay.kannan@kgisl.com>
2023-08-09 10:59:10 +0530

    adds common style sheet

---
+++ src/common/commonStyle.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export const commonStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
