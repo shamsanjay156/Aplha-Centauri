b9b174f7d506751c5e33b1601d38a16b22731cda
sanjay.kannan <sanjay.kannan@kgisl.com>
2023-08-09 10:47:58 +0530

    Eslint rules adds for inline style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        'no-inline-styles': false,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
 };
